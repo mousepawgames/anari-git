b7c3a155be20b0f43abda675dd9ea4dd5afc5087
Graham Mix <gacmix@mousepawmedia.com>
2020-09-10 10:23:29 -0700

    Moved around classes to fix weird { error

--- anari-source/include/anari/transformation_tests.hpp
+++ anari-source/include/anari/transformation_tests.hpp
@@ -32,45 +32,6 @@ public:
     }
 };
 
-class TransformationConstructorTester: public Test
-{
-public:
-    testdoc_t get_title()
-    {
-        return "Transformation Matrix3d Constructor";
-    }
-
-    testdoc_t get_docs()
-    {
-        return "Create a new Transformation object with a Matrix3d and test the constructor.";
-    }
-
-    bool run()
-    {
-        Matrix3d test_matrix; // Create a new test matrix
-
-        for (int i = 0; i < DIMENSION; i++)
-        {
-            for (int j = 0; j < DIMENSION; j++)
-            {
-                test_matrix(i, j) = i + j; // Random calculation of row# + col#
-            }
-
-        }
-        Transformation transform = Transformation(test_matrix);
-        for (int i = 0; i < DIMENSION; i++)
-        {
-            for (int j = 0; j < DIMENSION; j++)
-            {
-                PL_ASSERT_EQUAL(transform.the_matrix(i,j), test_matrix(i, j));  // Test to see if value inside index is calculated correctly from calculation above
-            }
-
-        }
-        return true;
-    }
-
-};
-
 class LinearTransformationTester: public Test
 {
 public:
@@ -108,6 +69,45 @@ public:
 
         }
         return true;
+};
+
+class TransformationConstructorTester: public Test
+{
+public:
+    testdoc_t get_title()
+    {
+        return "Transformation Matrix3d Constructor";
+    }
+
+    testdoc_t get_docs()
+    {
+        return "Create a new Transformation object with a Matrix3d and test the constructor.";
+    }
+
+    bool run()
+    {
+        Matrix3d test_matrix; // Create a new test matrix
+
+        for (int i = 0; i < DIMENSION; i++)
+        {
+            for (int j = 0; j < DIMENSION; j++)
+            {
+                test_matrix(i, j) = i + j; // Random calculation of row# + col#
+            }
+
+        }
+        Transformation transform = Transformation(test_matrix);
+        for (int i = 0; i < DIMENSION; i++)
+        {
+            for (int j = 0; j < DIMENSION; j++
+            {
+                PL_ASSERT_EQUAL(transform.the_matrix(i,j), test_matrix(i, j));  // Test to see if value inside index is calculated correctly from calculation above
+            }
+
+        }
+        return true;
+    }
+
 };
 
 class TestSuite_Transformation : public TestSuite
